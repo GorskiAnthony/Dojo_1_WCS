594c0c1da35639fda9ba78c5e1fdb521b9304ff8
Anthony Gorski <anthony.gorski@wildcodeschool.com>
2022-03-22 14:00:34 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Consignes
+ *
+ * Dojo : FizzBuzz
+ * affiche "fizz" si multiple de 3.
+ * affiche "buzz" si multiple de 5
+ * affiche "fizzbuzz" si c'est en même temps un multiple de 3 et 5
+ *
+ * Sans prompt
+ *
+ */
