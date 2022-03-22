b641550511bc1cd6a921b332a57cb0a10e4f830c
Anthony Gorski <anthony.gorski@wildcodeschool.com>
2022-03-22 15:12:45 +0100

    resolution

--- index.js
+++ index.js
@@ -9,3 +9,37 @@
  * Sans prompt
  *
  */
+
+// function dojo(number) {
+// 	if (number % 3 === 0 && number % 5 === 0) {
+// 		return "fizzbuzz";
+// 	} else if (number % 5 === 0) {
+// 		return "BUZZ";
+// 	} else if (number % 3 === 0) {
+// 		return "fizz";
+// 	} else {
+// 		return number;
+// 	}
+// }
+
+// console.log(dojo(7));
+// console.log(dojo(15));
+// console.log(dojo(9));
+// console.log(dojo(5));
+
+function dojo(number) {
+	switch (true) {
+		case number % 3 === 0 && number % 5 === 0:
+			return "fizzbuzz";
+		case number % 3 === 0:
+			return "fizz";
+		case number % 5 === 0:
+			return "buzz";
+		default:
+			return "invalid";
+	}
+}
+
+console.log(dojo("a"));
+console.log(dojo(3));
+console.log(dojo(15));
