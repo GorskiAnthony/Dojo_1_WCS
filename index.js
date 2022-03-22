533d3becd2b7a46f5e6b54be55f5b982a0b48a66
Anthony Gorski <anthony.gorski@wildcodeschool.com>
2022-03-22 15:12:06 +0100

    resolution

--- index.js
+++ index.js
@@ -9,3 +9,44 @@
  * Sans prompt
  *
  */
+
+/**
+ * En mode function
+ */
+
+// function fizzbuzz(multiples) {
+// 	if (multiples % 3 === 0 && multiples % 5 === 0) {
+// 		return "fizzbuzz";
+// 	} else if (multiples % 3 === 0) {
+// 		return "fizz";
+// 	} else if (multiples % 5 === 0) {
+// 		return "buzz";
+// 	} else {
+// 		return "Aucune des 3 réponses ";
+// 	}
+// }
+
+// console.log(fizzbuzz(4));
+// console.log(fizzbuzz(20));
+// console.log(fizzbuzz(3));
+// console.log(fizzbuzz(552145215));
+
+/**
+ * En mode Switch / Case
+ */
+
+const multiples = 15;
+
+switch (true) {
+	case multiples % 3 === 0 && multiples % 5 === 0:
+		console.log("fizzbuzz");
+		break;
+	case multiples % 3 === 0:
+		console.log("fizz");
+		break;
+	case multiples % 5 === 0:
+		console.log("buzz");
+		break;
+	default:
+		console.log("Aucune des 3 réponses");
+}
